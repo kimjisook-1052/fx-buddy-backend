2089d145d1089b7b19fde2e16f614150bfcddcd1
Kimjisook <jisookkim1052@gmail.com>
2026-09-03 18:47:58 +0900

    fix CORS credentials setting

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ allowed_origins = os.getenv("ALLOWED_ORIGINS", "*").split(",")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=allowed_origins,
-    allow_credentials=True,
+    allow_credentials=F,
     allow_methods=["*"],
     allow_headers=["*"],
 )
